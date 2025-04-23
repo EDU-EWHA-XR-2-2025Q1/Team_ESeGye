d53d40e753809e1234dbc08084fa6f6d1d25ca57
sohee <soheeaubrey@ewha.ac.kr>
2025-04-23 11:49:53 +0900

    Feat: JSH Scene 작업 완료

---
+++ TW01/Assets/TW01/Assets/TW01/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+
+    public void LoadYVJScene()
+    {
+        SceneManager.LoadScene("TW01_YVJ_Scene");
+    }
+
+    public void LoadJSHScene(){
+
+        Debug.Log("버튼 눌림 - 씬 이동 시도");
+
+        if (Application.CanStreamedLevelBeLoaded("TW01_JSH_Scene"))
+        {
+            Debug.Log("씬 전환 시작!");
+            SceneManager.LoadScene("TW01_JSH_Scene");
+        }
+        else
+        {
+            Debug.LogError("TW01_JYJ_Scene 씬이 Build Settings에 등록되지 않았거나 이름이 틀렸습니다!");
+        }
+    }
+}
---
+++ TW01/Assets/TW01/Assets/TW01/TW01_JYJ/Scripts_JYJ/Item_Controller.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Item_Controller : MonoBehaviour
+{
+    private bool isPicked = false;
+
+    void OnMouseDown()
+    {
+        if (isPicked) return;
+
+        isPicked = true;
+
+        Debug.Log("Pick!");
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.isKinematic = true; 
+
+        Transform cam = Camera.main.transform;
+        Vector3 holdPosition = cam.position + cam.forward * 1.5f + Vector3.down * 0.2f;
+
+        transform.position = holdPosition;
+        transform.rotation = cam.rotation;
+
+        transform.SetParent(cam);
+
+    }
+
+}
