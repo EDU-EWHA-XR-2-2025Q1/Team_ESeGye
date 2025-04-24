b231e7358f61991311084f960a85aad790647b55
Dongseon Yang <ydsperfect08@ewhain.net>
2025-04-24 23:11:19 +0900

    Feat : YDS renaming

---
+++ TW01/Assets/TW01/Assets/TW01/TW01_YDS/TW01_YDS_Basket_Controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TW01_YDS_Basket_Controller : MonoBehaviour
+{
+    public GameObject UI_Controller;
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+
+        if (other.CompareTag("Item"))
+
+        {
+
+            Destroy(other.gameObject);
+
+        }
+
+    }
+}
